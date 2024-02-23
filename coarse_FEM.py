e5de04e76b34aa6b940fb385747fc9a037a6e186
soloplay0510 <33068129+soloplay0510@users.noreply.github.com>
2024-02-22 18:35:52 -0600

    Create coarse_FEM.py

---
+++ coarse_FEM.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 12:59:45 2024
+
+@author: min
+"""
+
+import fenics
+
+
+class near_Gamma():
+    
+    
+class away_Gamma
